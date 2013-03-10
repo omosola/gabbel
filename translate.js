60dfadf39892d0cbcf406eb1b733e7002348ba74
Omosola <omosolao@stanford.edu>
2013-03-10 04:05:35 -0700

    Translation up and running. Still need to test and get it working well in real time. Also see how quickly it can transcribe text.

---
+++ translate.js
@@ -0,0 +1,30 @@
+var url = require('url');
+// source - https://github.com/nanek/mstranslator //
+var MsTranslator = require('mstranslator');
+// TODO: Make this info private //
+var client = new MsTranslator({
+        client_id: "Solaode1",
+        client_secret: "c38SiJaHmSS4ov8H/EVrmG0ps+rNZ/BgeDLUyCmJPXA="
+    });
+
+
+exports.translate = function(text, source_lang, dest_lang) {
+    var params = {
+	text: text,
+	from: source_lang,
+	to: dest_lang
+    };
+    console.log("From: " +params.from);
+    console.log("To: " + params.to);
+    console.log("Text: " + params.text);
+
+    var translation = ""
+
+    client.initialize_token(function(keys) {
+	    client.translate(params, function(err, data) {
+		    translation = data;
+		    console.log(translation);
+	      		});
+	});
+    return translation;
+}
